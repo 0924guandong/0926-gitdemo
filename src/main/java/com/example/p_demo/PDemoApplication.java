ec44b07fd6bb6e825b2513b5e28949acbc30aef7
guandong <347070087@qq.com>
2020-09-26 13:19:31 +0800

    p_demo 提交

---
+++ src/main/java/com/example/p_demo/PDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.p_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/p_demo/PDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.p_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
